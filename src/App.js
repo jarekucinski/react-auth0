34a71f03e1026cbbd763dd224e73f1ff6e670b6e
jarekucinski <jucinski@wp.pl>
2019-06-21 22:02:06 +0200

    Add Private API

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Profile from "./Profile";
 import Nav from "./Nav";
 import Auth from "./auth/Auth";
 import Callback from "./Callback";
+import Public from "./Public";
+import Private from "./Private";
 
 class App extends Component {
   constructor(props) {
@@ -33,6 +35,17 @@ class App extends Component {
                 <Profile auth={this.auth} {...props} />
               ) : (
                 <Redirect to="/" />
+              )
+            }
+          />
+          <Route path="/public" component={Public} />
+          <Route
+            path="/private"
+            render={props =>
+              this.auth.isAuthenticated() ? (
+                <Private auth={this.auth} {...props} />
+              ) : (
+                this.auth.login()
               )
             }
           />
